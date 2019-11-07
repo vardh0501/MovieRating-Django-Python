9009235e9a3cc1173135e5024f8fe90f54a9fbb1
vishnu <vardhan.vishnu037@gmail.com>
2019-11-06 21:59:09 -0500

    login.html

--- masterdata/admin.py
+++ masterdata/admin.py
@@ -4,6 +4,7 @@ from masterdata.models import Movie,Profile,MovieFeedback
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('m_name','d_name','actor','movie_id','budget')
 
+
 admin.site.register(Movie,MovieAdmin)
 admin.site.register(Profile)
 admin.site.register(MovieFeedback)
--- masterdata/views.py
+++ masterdata/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from masterdata.models import Movie, Profile, MovieFeedback
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 
 def index(request):
+    #import ipdb;ipdb.set_trace()
     movie_obj = Movie.objects.all()
     profile_obj = Profile.objects.all()
     moviefeed_obj = MovieFeedback.objects.all()
     return render(request, 'index.html', locals())
     
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', locals())
+    else:
+        #import ipdb;ipdb.set_trace()
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return HttpResponseRedirect("/home")
+        else:
+            message = "failure"
+        return render(request, 'login.html', locals())
+
+        #return HttpResponse(username+password)
